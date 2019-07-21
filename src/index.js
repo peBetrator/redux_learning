aa7e3416e8f2229d1fc0b178439f3aa070b03fad
peBetrator <fivalit@gmail.com>
2019-07-21 15:07:41 +0300

    redux with NO react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import { createStore } from "redux";
-
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-    default:
-      return state;
-  }
-};
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as actions from "./actions";
 
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
+const { dispatch } = store;
+
+/**
+ * manually created 'bindActionCreator'
+const bindActionCreator = (creator, dispatch) => (...args) => {
+  dispatch(creator(...args));
+};
+*/
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+document.getElementById("inc").addEventListener("click", inc);
+
+document.getElementById("dec").addEventListener("click", dec);
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const payload = Math.floor(Math.random() * 10);
+  rnd(payload);
 });
 
-store.dispatch({ type: "INC" });
-store.dispatch({ type: "INC" });
+const update = () => {
+  document.getElementById("counter").innerHTML = store.getState();
+};
+
+store.subscribe(update);
